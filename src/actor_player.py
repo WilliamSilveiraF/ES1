0c68704d4eaab010a30642809ab9ec95c53c9eec
William Silveira <williamsilveiraf@hotmail.com>
2023-05-11 18:48:54 -0300

    chore: increase_salary_10_percent, add_child, handle_default_turn_income, apply_income_tax

--- src/actor_player.py
+++ src/actor_player.py
@@ -23,50 +23,81 @@ class Jogador:
         self.cor = cor
         self.posicao = posicao
         self.salario = salario
-        self.dinheiro = 0
+        self.child_amount = 0
+        self.dinheiro = 100000
+        self.is_retired = False
+        self.is_playing = True
+        self.is_life_insured = False
+        self.is_vehicle_insured = False
 
+    def increase_salary_10_percent(self):
+        self.salario *= 1.1
+    
+    def add_child(self):
+        if self.child_amount == 4:
+            raise TypeError("Child amount limit is 4")
+        self.child_amount += 1
+
+    def handle_default_turn_income(self):
+        COST_BY_CHILD = -500
+        transaction = self.salario
+
+        if self.is_retired:
+            transaction += 5000
+
+        transaction += self.child_amount * COST_BY_CHILD
+
+        self.dinheiro += transaction
+
+    def apply_income_tax(self):
+        BASE = 0.7
+        self.dinheiro *= BASE
+    
+
+    
 class Casa(Enum):
-    INIT = (1, 'Init House', 'Init House Description')
-    GRADUATION = (2, 'You graduated', 'Congratulations, you graduated!! With that you must pay tuition of U$$ 20,000 from your college.')
-    BIRTH = (3, 'Birth', 'Congratulations, a new child has joined your family!. Remember, with every joy comes new responsibilities. Each turn you must pay U$$ 500 by child.')
-    LOTTERY = (4, 'Lottery', "You've won the lottery! Collect an immediate U$$ 75,000 bonus to represent your winnings.")
-    MOUNT_EVEREST = (5, 'Mount Everest Expedition', "You've decided to climb Mount Everest! Pay U$$ 5,000 to cover the expedition costs, but earn life points for the achievement.")
-    PROMOTION = (6, 'Promotion', 'Congratulations, your salary increases by 10 percent.')
-    ADOPTION = (7, 'Adoption', 'Your heart and home have expanded with the adoption of a child. Celebrate this moment and remember, every child is a bundle of joy and a responsibility to shape the future. Each turn you must pay U$$ 500 by child.')
-    ROBBERY = (8, 'Robbery', "Unfortunately, you've been robbed. Pay U$$ 10,000 to represent the financial loss.")
-    BACKPACKING = (9, 'Backpacking Trip', "You've decided to go backpacking through Europe! Pay U$$ 15,000 to cover the trip costs.")
-    INTERNSHIP = (10, 'Internship', 'You have landed an internship in your field! This is a great step towards your career. You will gain valuable experience, but you will earn only half your salary this turn as internships are typically low-paying.')
-    CHILD_GRADUATION = (11, "Child's Graduation", 'Your child has hit a significant milestone - university graduation! This proud moment comes with expenses. Make a U$$ 5,000 payment to cover the cost of the graduation ceremony, from the gown to the celebratory dinner.')
-    INVESTMENT = (12, 'Investment', "You've decided to invest some money. You will gain U$$ 12,500.")
-    CORAL_REEF_DIVE = (13, 'Coral Reef Dive', "You've decided to dive in the Coral Reef! Pay U$$ 6,000 to cover the dive costs.")
-    POST_GRADUATION = (14, 'Post-Graduation', 'You have completed your postgraduate studies! This milestone is an important step in your career and deserves a reward. Collect U$$ 10,000 bonus to reflect your increased qualifications.')
-    SCHOOL_CHANGE = (15, 'School Change', "Your child's education journey requires a school change. Whether it's due to a move or just seeking better opportunities, there are costs involved. Pay U$$ 7,000 for the move, new uniforms, and other related expenses.")
-    LIFE_INSURANCE = (16, 'Life Insurance', "You've decided to buy life insurance. Pay U$$ 10,000 to represent the insurance premium.")
-    MARATHON = (17, 'Marathon', "You've decided to run a marathon! Pay U$$ 2,500 to cover the registration and training costs, but earn life points for the achievement.")
-    ENTREPRENEUR = (18, 'Entrepreneur', 'You have taken the bold step of starting your own business! This exciting venture comes with its costs. Pay U$$ 25,000 to cover the startup costs.')
-    FAMILY_TRIP = (19, 'Family Trip', "Quality time alert! You're taking a well-deserved family trip. It's time for relaxation and adventures. Pay U$$ 12,000 for the trip expenses, including travel, accommodation, and daily allowances.")
-    INHERITANCE = (20, 'Inheritance', "You've inherited a large sum of money. Collect U$$ 50,000 bonus to represent the inheritance.")
-    SPACE_TRAVEL = (21, 'Space Travel', "You've decided to go to space! Pay U$$ 30,000 to cover the travel costs.")
-    RETIREMENT = (22, 'Retirement', "You've retired after a long and successful career! It's time to relax and enjoy the fruits of your labor. Now, you will collect U$$ 5,000 bonus each turn.")
-    CHILDREN_WEDDING = (23, "Children's Wedding", "Your child is getting married! A proud and emotional moment for you. However, weddings can be expensive. Pay U$$ 17,500 for the wedding party, from the venue to the food and the decorations.")
-    CAR_INSURANCE = (24, 'Car Insurance', "You've decided to buy car insurance. Pay U$$ 8,000 amount to represent the insurance premium.")
-    AFRICAN_SAFARI = (25, 'African Safari', "You've decided to go on a safari in Africa! Pay U$$ 16,000 amount to cover the trip costs.")
-    CAREER_CHANGE = (26, 'Career Change', 'You have decided to change your career! This brave move opens up new opportunities, your salary increases by 20 percent.')
-    MUSIC_LESSON = (27, 'Music Lesson', 'Your children have shown an interest in music and want to learn an instrument. This could be the start of a lifelong passion or even a career. Pay U$$ 2,500 for the music lessons, including the cost of the instrument and the tutor.')
-    INCOME_TAX = (28, 'Income Tax', "It's income tax time. Pay 30 percent of your total money to represent your taxes.")
-    ANTARCTIC_EXPEDITION = (29, 'Antarctic Expedition', "You've decided to visit Antarctica! Pay U$$ 22,500 to cover the trip costs.")
-    VOLUNTEER_WORK = (30, 'Volunteer Work', 'You have decided to do volunteer work. This noble act might not provide a salary this turn, but it brings a great sense of fulfillment.')
-    SPORTS_COMPETITION = (31, 'Sports Competition', "Your children have entered a sports competition! They're excited and nervous. Pay U$$ 3,000 for the sports equipment, team uniforms, and other related expenses.")
-    CHARITY_HOUSE = (32, 'Charity House', "You've decided to make a generous donation to a charity that's close to your heart. Pay U$$ 10,000 amount to represent your charitable contribution.")
-    MOTORCYCLE_JOURNEY = (33, 'Motorcycle Journey on Route 66', "You've decided to ride Route 66 on a motorcycle! Pay U$$ 8,000 to cover the trip costs.")
-    REMOTE_WORK = (34, 'Remote Work', 'You have landed the opportunity to work remotely! This gives you more flexibility and time to spend with family or on hobbies. Receive your normal salary this turn and gain an immediate bonus to represent the savings on transportation and work clothes.')
-    BIRTHDAY_PARTY = (35, 'Birthday Party', "It's time to celebrate! One of your children has a birthday. You're throwing a party complete with cake, games, and party favors. Pay U$$ 7,500 for the party expenses.")
-    WINDFALL = (36, 'Windfall', "You've received an unexpected windfall, perhaps from a forgotten investment or a distant relative. Collect U$$ 20,000 bonus to represent the unexpected influx of money.")
+    INIT = (1, 'Init House', 'Init House Description', 0)
+    GRADUATION = (2, 'You graduated', 'Congratulations, you graduated!! With that you must pay tuition of U$$ 20,000 from your college.', -20000)
+    BIRTH = (3, 'Birth', 'Congratulations, a new child has joined your family!. Remember, with every joy comes new responsibilities. Each turn you must pay U$$ 500 by child.', 0)
+    LOTTERY = (4, 'Lottery', "You've won the lottery! Collect an immediate U$$ 75,000 bonus to represent your winnings.", 75000)
+    MOUNT_EVEREST = (5, 'Mount Everest Expedition', "You've decided to climb Mount Everest! Pay U$$ 5,000 to cover the expedition costs.", -5000)
+    PROMOTION = (6, 'Promotion', 'Congratulations, your salary increases by 10 percent.', 0)
+    ADOPTION = (7, 'Adoption', 'Your heart and home have expanded with the adoption of a child. Celebrate this moment and remember, every child is a bundle of joy and a responsibility to shape the future. Each turn you must pay U$$ 500 by child.', 0)
+    ROBBERY = (8, 'Robbery', "Unfortunately, you've been robbed. Pay U$$ 10,000 to represent the financial loss.", -10000)
+    BACKPACKING = (9, 'Backpacking Trip', "You've decided to go backpacking through Europe! Pay U$$ 15,000 to cover the trip costs.", -15000)
+    INTERNSHIP = (10, 'Internship', 'You have landed an internship in your field! This is a great step towards your career. You will gain valuable experience, but you will earn only half your salary this turn as internships are typically low-paying.', 0)
+    CHILD_GRADUATION = (11, "Child's Graduation", 'Your child has hit a significant milestone - university graduation! This proud moment comes with expenses. Make a U$$ 5,000 payment to cover the cost of the graduation ceremony, from the gown to the celebratory dinner.', -5000)
+    INVESTMENT = (12, 'Investment', "You've decided to invest some money. You will gain U$$ 12,500.", 12500)
+    CORAL_REEF_DIVE = (13, 'Coral Reef Dive', "You've decided to dive in the Coral Reef! Pay U$$ 6,000 to cover the dive costs.", -6000)
+    POST_GRADUATION = (14, 'Post-Graduation', 'You have completed your postgraduate studies! This milestone is an important step in your career and deserves a reward. Collect U$$ 10,000 bonus to reflect your increased qualifications.', 10000)
+    SCHOOL_CHANGE = (15, 'School Change', "Your child's education journey requires a school change. Whether it's due to a move or just seeking better opportunities, there are costs involved. Pay U$$ 7,000 for the move, new uniforms, and other related expenses.", -7000)
+    LIFE_INSURANCE = (16, 'Life Insurance', "You've decided to buy life insurance. Pay U$$ 10,000 to represent the insurance premium.", -10000) #CHECK 
+    MARATHON = (17, 'Marathon', "You've decided to run a marathon! Pay U$$ 2,500 to cover the registration and training costs.", -2500)
+    ENTREPRENEUR = (18, 'Entrepreneur', 'You have taken the bold step of starting your own business! This exciting venture comes with its costs. Pay U$$ 25,000 to cover the startup costs.', -25000)
+    FAMILY_TRIP = (19, 'Family Trip', "Quality time alert! You're taking a well-deserved family trip. It's time for relaxation and adventures. Pay U$$ 12,000 for the trip expenses, including travel, accommodation, and daily allowances.", -12000)
+    INHERITANCE = (20, 'Inheritance', "You've inherited a large sum of money. Collect U$$ 100,000 bonus to represent the inheritance.", 100000)
+    SPACE_TRAVEL = (21, 'Space Travel', "You've decided to go to space! Pay U$$ 30,000 to cover the travel costs.", -30000)
+    RETIREMENT = (22, 'Retirement', "You've retired after a long and successful career! It's time to relax and enjoy the fruits of your labor. Now, you will collect U$$ 5,000 bonus each turn.", 0)
+    CHILDREN_WEDDING = (23, "Children's Wedding", "Your child is getting married! A proud and emotional moment for you. However, weddings can be expensive. Pay U$$ 17,500 for the wedding party, from the venue to the food and the decorations.", -17500)
+    CAR_INSURANCE = (24, 'Car Insurance', "You've decided to buy car insurance. Pay U$$ 8,000 amount to represent the insurance premium.", -8000)
+    AFRICAN_SAFARI = (25, 'African Safari', "You've decided to go on a safari in Africa! Pay U$$ 16,000 amount to cover the trip costs.", -16000)
+    CAREER_CHANGE = (26, 'Career Change', 'You have decided to change your career! This brave move opens up new opportunities, your salary increases by 20 percent.', 0)
+    MUSIC_LESSON = (27, 'Music Lesson', 'Your children have shown an interest in music and want to learn an instrument. This could be the start of a lifelong passion or even a career. Pay U$$ 2,500 for the music lessons, including the cost of the instrument and the tutor.', -2500)
+    INCOME_TAX = (28, 'Income Tax', "It's income tax time. Pay 30 percent of your total money to represent your taxes.", 0)
+    ANTARCTIC_EXPEDITION = (29, 'Antarctic Expedition', "You've decided to visit Antarctica! Pay U$$ 22,500 to cover the trip costs.", -22500)
+    VOLUNTEER_WORK = (30, 'Volunteer Work', 'You have decided to do volunteer work. This noble act might not provide a salary this turn, but it brings a great sense of fulfillment.', 0)
+    SPORTS_COMPETITION = (31, 'Sports Competition', "Your children have entered a sports competition! They're excited and nervous. Pay U$$ 3,000 for the sports equipment, team uniforms, and other related expenses.", -3000)
+    CHARITY_HOUSE = (32, 'Charity House', "You've decided to make a generous donation to a charity that's close to your heart. Pay U$$ 10,000 amount to represent your charitable contribution.", -10000)
+    MOTORCYCLE_JOURNEY = (33, 'Motorcycle Journey on Route 66', "You've decided to ride Route 66 on a motorcycle! Pay U$$ 8,000 to cover the trip costs.", -8000)
+    REMOTE_WORK = (34, 'Remote Work', 'You have landed the opportunity to work remotely! This gives you more flexibility and time to spend with family or on hobbies. Receive your normal salary this turn and gain U$$ 3,000 bonus to represent the savings on transportation and work clothes.', 3000)
+    BIRTHDAY_PARTY = (35, 'Birthday Party', "It's time to celebrate! One of your children has a birthday. You're throwing a party complete with cake, games, and party favors. Pay U$$ 7,500 for the party expenses.", -7500)
+    WINDFALL = (36, 'Windfall', "You've received an unexpected windfall, perhaps from a forgotten investment or a distant relative. Collect U$$ 20,000 bonus to represent the unexpected influx of money.", 20000)
 
-    def __init__(self, posicao, title, description):
+    def __init__(self, posicao, title, description, transaction):
         self.posicao = posicao
         self.title = title
         self.description = description
+        self.transaction = transaction
 
 class ActorPlayer(DogPlayerInterface):
     def __init__(self, master):
@@ -380,14 +411,13 @@ class GameInterface:
             print("Button 1 clicked")
 
         new_pos = jogador.posicao
-        
+        print(new_pos)
         buttons = [{"text": "OK", "command": close_custom_dialog}]
 
         for casa in Casa:
             if new_pos == casa.posicao:
-                title = casa.title
-                description = casa.description
-                d = CustomDialog(self.master, title=title, message=description, buttons=buttons)
+                
+                d = CustomDialog(self.master, title=casa.title, message=casa.description, buttons=buttons)
                 break
         #d = CustomDialog(self.master, title="You landed on a special casa!", 
         #                    message=f"{self.player_name} landed on casa 5.", buttons=buttons)
