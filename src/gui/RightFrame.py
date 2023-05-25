c37ebf265f9df25dd3cad4a545aa5dde48f45b7d
William Silveira <williamsilveiraf@hotmail.com>
2023-05-24 21:32:46 -0300

    refactor: CardFactory

--- src/gui/RightFrame.py
+++ src/gui/RightFrame.py
@@ -2,18 +2,45 @@ from tkinter import *
 import tkinter as tk
 from logic.Player import Player
 from contants import ALTURA_TABULEIRO
+from typing import List
+from collections import namedtuple
 
+Card = namedtuple("Card", ["field", "value"])
 
-class RightFrame:
-    def __init__(self, master, width, height, players: list[Player]):
-        self.right_frame = tk.Canvas(master, width=width, height=height, bg='white')
-        self.right_frame.pack(side="right")
+class CardFactory:
+    def __init__(self, frame, board_height, players: list[Player]):
+        self.frame = frame
+        self.card_width = int(0.8 * 300)
+        self.card_height = int(board_height / 4)
+        self.card_space = int((board_height - 3 * self.card_height) / 4)
+        self.x1 = (300 - self.card_width) // 2
+        self.card_attributes = ['#ebf8ff', '#70cdf6', '#aadef5', '#043c50']
+        self.card_texts = self.create_cards(players)
 
-        self.first_card_texts = []
-        self.second_card_texts = []
-        self.third_card_texts = []
+    def _create_card(self, player: Player, i: int) -> List[Card]:
+        y1 = self.card_space * (i + 1) + self.card_height * i
+        x2 = self.x1 + self.card_width
+        y2 = y1 + self.card_height
+        # shadow
+        self._create_rounded_rect(self.x1+5, y1+5, x2+5, y2+5, radius=20, fill=self.card_attributes[0])
+        # card
+        self._create_rounded_rect(self.x1, y1, x2, y2, radius=20, fill=self.card_attributes[1], outline=self.card_attributes[2])
+        self.frame.create_text(self.x1 + 10, y1 + 10, text=f'#{player.player_id}', anchor="nw", font=("Arial", 14, "bold"), fill=self.card_attributes[3])
+        card_content = player.get_card_content()
+        card_texts = []
+        y_start = self.card_space * (i + 1) + self.card_height * i + 40
+        for line in card_content:
+            field_text = self.frame.create_text(self.x1 + 10, y_start, text=line['field'], anchor="nw", font=("Arial", 12, "bold"), fill='#22471a')
+            value_text = self.frame.create_text(self.x1 + self.card_width - 10, y_start, text=line['value'], anchor="ne", font=("Arial", 12), fill='#22471a')
+            card_texts.append(Card(field_text, value_text))
+            y_start += 22
+        return card_texts
 
-        self._create_cards(players)
+    def create_cards(self, players: List[Player]):
+        card_texts = []
+        for i, player in enumerate(players):
+            card_texts.append(self._create_card(player, i))
+        return card_texts
 
     def _create_rounded_rect(self, x1, y1, x2, y2, radius=25, **kwargs):
         """Draw a rounded rectangle"""
@@ -37,65 +64,13 @@ class RightFrame:
                 x1, y1+radius,
                 x1, y1+radius,
                 x1, y1]
-        return self.right_frame.create_polygon(points, **kwargs, smooth=True)
+        return self.frame.create_polygon(points, **kwargs, smooth=True)
 
-    def _create_cards(self, players: list[Player]):
-        self.first_card_texts = []
-        self.second_card_texts = []
-        self.third_card_texts = []
-        
-        card_width = int(0.8 * 300)
-        card_height = int(ALTURA_TABULEIRO / 4)
-        card_space = int((ALTURA_TABULEIRO - 3 * card_height) / 4)
+class RightFrame:
+    def __init__(self, master, width, height, players: list[Player]):
+        self.right_frame = tk.Canvas(master, width=width, height=height, bg='white')
+        self.right_frame.pack(side="right")
+        self.cards = CardFactory(self.right_frame, ALTURA_TABULEIRO, players)
 
-        x1 = (300 - card_width) // 2
-        [shadow_color, card_bg, card_outline, card_title] = ['#ebf8ff', '#70cdf6', '#aadef5', '#043c50']
-
-        for i in range(3):
-            y1 = card_space * (i + 1) + card_height * i
-            x2 = x1 + card_width
-            y2 = y1 + card_height
-            # shadow
-            self._create_rounded_rect(x1+5, y1+5, x2+5, y2+5, radius=20, fill=shadow_color)
-            # card
-            self._create_rounded_rect(x1, y1, x2, y2, radius=20, fill=card_bg, outline=card_outline)
-            self.right_frame.create_text(x1 + 10, y1 + 10, text=f'#{players[i].player_id}', anchor="nw", font=("Arial", 14, "bold"), fill=card_title)
-
-        first_player_card_lines = players[0].get_card_content()
-        second_player_card_lines = players[1].get_card_content()
-        third_player_card_lines = players[2].get_card_content()
-
-        y_start = card_space + 40
-
-        for line in first_player_card_lines:
-            field_text = self.right_frame.create_text(x1 + 10, y_start, text=line['field'], anchor="nw", font=("Arial", 12, "bold"), fill='#22471a')
-            value_text = self.right_frame.create_text(x1 + card_width - 10, y_start, text=line['value'], anchor="ne", font=("Arial", 12), fill='#22471a')
-            self.first_card_texts.append((field_text, value_text))
-            y_start += 22
-
-        y_start = 2 * card_space + card_height + 40
-
-        for line in second_player_card_lines:
-            field_text = self.right_frame.create_text(x1 + 10, y_start, text=line['field'], anchor="nw", font=("Arial", 12, "bold"), fill='#043c50')
-            value_text = self.right_frame.create_text(x1 + card_width - 10, y_start, text=line['value'], anchor="ne", font=("Arial", 12), fill='#043c50')
-            self.second_card_texts.append((field_text, value_text))
-            y_start += 22
-
-        y_start = 3 * card_space + 2 * card_height + 40
-
-        for line in third_player_card_lines:
-            self.right_frame.create_text(x1 + 10, y_start, text=line['field'], anchor="nw", font=("Arial", 12, "bold"), fill='#043c50')
-            self.right_frame.create_text(x1 + card_width - 10, y_start, text=line['value'], anchor="ne", font=("Arial", 12), fill='#043c50')
-            y_start += 22
-
-    def _update_card(self, card_number, new_lines):
-        if card_number == 1:
-            texts = self.first_card_texts
-        elif card_number == 2:
-            texts = self.second_card_texts
-        else:
-            texts = self.third_card_texts
-
-        for i, line in enumerate(new_lines):
-            self.right_frame.itemconfig(texts[i][0], text=line['field'])
-            self.right_frame.itemconfig(texts[i][1], text=line['value'])
+    def _update_card(self, players: list[Player]):
+        pass
